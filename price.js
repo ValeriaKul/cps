20ac5999ece2fada4ae26fa9b870291f421f68a4
Valeriia Kulikova <lera.dribnyuk@mail.ru>
2025-06-02 14:42:55 +0100

    changed grid murkup in the PricePage

--- price.js
+++ price.js
@@ -6,29 +6,29 @@ const html = `
     <p class="brands__title-text price__title-text">Цены на услуги</p>
   </div>
   <div class="prices__header">
-    <span>Ремонтные услуги</span>
-    <span>Цена</span>
-    <span>Срок</span>
-    <span></span>
+    <span class="item-1">Ремонтные услуги</span>
+    <span class="item-2">Цена</span>
+    <span class="item-3">Срок</span>
+    <span class="item-4"></span>
   </div>
   <!-- !slider -->
   <div class="swiper prices-slider">
     <div class="swiper-wrapper price-wrapper">
       <div class="swiper-slide prices-slide">
         <div class="prices-slide__item">
-          <div class="slide__group">
+          <div class="slide__group item-1">
             <span class="slide__label">Ремонтные услуги</span>
             <p class="slide__value">Диагностика</p>
           </div>
-          <div class="slide__group">
+          <div class="slide__group item-2">
             <span class="slide__label">Цена</span>
             <p class="slide__value">Бесплатно</p>
           </div>
-          <div class="slide__group">
+          <div class="slide__group item-3">
             <span class="slide__label">Срок</span>
             <p class="slide__value">30 мин</p>
           </div>
-          <div class="slide__group">
+          <div class="slide__group item-4">
             <button class="slide__button">
               Заказать <img src="./images/icons/goside.svg" alt="arrow" />
             </button>
@@ -37,19 +37,19 @@ const html = `
       </div>
       <div class="swiper-slide prices-slide">
         <div class="prices-slide__item">
-          <div class="slide__group">
+          <div class="slide__group item-1">
             <span class="slide__label">Ремонтные услуги</span>
             <p class="slide__value">Замена дисплея</p>
           </div>
-          <div class="slide__group">
+          <div class="slide__group item-2">
             <span class="slide__label">Цена</span>
             <p class="slide__value">1000 ₽</p>
           </div>
-          <div class="slide__group">
+          <div class="slide__group item-3">
             <span class="slide__label">Срок</span>
             <p class="slide__value">30–120 мин</p>
           </div>
-          <div class="slide__group">
+          <div class="slide__group item-4">
             <button class="slide__button">
               Заказать <img src="./images/icons/goside.svg" alt="arrow" />
             </button>
@@ -58,19 +58,19 @@ const html = `
       </div>
       <div class="swiper-slide prices-slide">
         <div class="prices-slide__item">
-          <div class="slide__group">
+          <div class="slide__group item-1">
             <span class="slide__label">Ремонтные услуги</span>
             <p class="slide__value">Замена полифонического динамика</p>
           </div>
-          <div class="slide__group">
+          <div class="slide__group item-2">
             <span class="slide__label">Цена</span>
             <p class="slide__value">1000 ₽</p>
           </div>
-          <div class="slide__group">
+          <div class="slide__group item-3">
             <span class="slide__label">Срок</span>
             <p class="slide__value">30–120 мин</p>
           </div>
-          <div class="slide__group">
+          <div class="slide__group item-4">
             <button class="slide__button">
               Заказать <img src="./images/icons/goside.svg" alt="arrow" />
             </button>
@@ -79,21 +79,21 @@ const html = `
       </div>
       <div class="swiper-slide prices-slide">
         <div class="prices-slide__item">
-          <div class="slide__group">
+          <div class="slide__group item-1">
             <span class="slide__label">Ремонтные услуги</span>
             <p class="slide__value">
               Тестирование с выдачей технического заключения
             </p>
           </div>
-          <div class="slide__group">
+          <div class="slide__group item-2">
             <span class="slide__label">Цена</span>
             <p class="slide__value">1000 ₽</p>
           </div>
-          <div class="slide__group">
+          <div class="slide__group item-3">
             <span class="slide__label">Срок</span>
             <p class="slide__value">30–120 мин</p>
           </div>
-          <div class="slide__group">
+          <div class="slide__group item-4">
             <button class="slide__button">
               Заказать <img src="./images/icons/goside.svg" alt="arrow" />
             </button>
@@ -102,19 +102,19 @@ const html = `
       </div>
       <div class="swiper-slide prices-slide">
         <div class="prices-slide__item">
-          <div class="slide__group">
+          <div class="slide__group item-1">
             <span class="slide__label">Ремонтные услуги</span>
             <p class="slide__value">Замена программного обеспечения</p>
           </div>
-          <div class="slide__group">
+          <div class="slide__group item-2">
             <span class="slide__label">Цена</span>
             <p class="slide__value">1000 ₽</p>
           </div>
-          <div class="slide__group">
+          <div class="slide__group item-3">
             <span class="slide__label">Срок</span>
             <p class="slide__value">30–120 мин</p>
           </div>
-          <div class="slide__group">
+          <div class="slide__group item-4">
             <button class="slide__button">
               Заказать <img src="./images/icons/goside.svg" alt="arrow" />
             </button>
